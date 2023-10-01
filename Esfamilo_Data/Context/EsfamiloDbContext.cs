f79f71e3f68dbed14acb003820a695b695462aff
farzadflr <fheydari207@gmail.com>
2023-10-01 20:09:46 +0330

    add data context in data layer

---
+++ Esfamilo_Data/Context/EsfamiloDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Esfamilo_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfamilo_Data.Context
+{
+    public class EsfamiloDbContext : DbContext
+    {
+        public EsfamiloDbContext(DbContextOptions<EsfamiloDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Lobby> Lobbies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Friend> friends { get; set; }
+        public DbSet<UserInLobby> userInLobbies { get; set; }
+        public DbSet<WordForCategory> WordForCategories { get; set; }
+    }
+}
