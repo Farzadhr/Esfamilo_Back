dd7f56a180249767f3e3eae2e6cdb218856c6b6d
farzadflr <fheydari207@gmail.com>
2023-10-01 20:26:14 +0330

    Add interface Repository in Domain

---
+++ Esfamilo_Domain/Interfaces/Generic/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfamilo_Domain.Interfaces.Generic
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Add(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<T> Get(T entity);
+        Task<IEnumerable<T>> GetAll();
+
+    }
+}
---
+++ Esfamilo_Domain/Interfaces/IFriendRepository.cs
@@ -0,0 +1,14 @@
+﻿using Esfamilo_Domain.Interfaces.Generic;
+using Esfamilo_Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfamilo_Domain.Interfaces
+{
+    public interface IFriendRepository : IGenericRepository<Friend>
+    {
+    }
+}
